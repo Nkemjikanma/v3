af776c1edfaa5229a7cec0eb79d8dacf5fe64696
nkemjika <nkemjikaohanyere@gmail.com>
2026-04-01 16:23:26 +0100

    fix: fix route for steps

--- be/src/routes/steps.rs
+++ be/src/routes/steps.rs
@@ -5,13 +5,10 @@ use actix_web_httpauth::middleware::HttpAuthentication;
 
 pub fn configure_steps(cfg: &mut web::ServiceConfig) {
     let auth_middleware = HttpAuthentication::bearer(validator);
-    cfg.service(
-        web::scope("/steps")
-            .route("", web::get().to(steps::get_steps))
-            .service(
-                web::scope("/admin/steps")
-                    .wrap(auth_middleware)
-                    .route("", web::post().to(steps::set_steps)),
-            ),
-    );
+    cfg.service(web::scope("/steps").route("", web::get().to(steps::get_steps)))
+        .service(
+            web::scope("/admin/steps")
+                .wrap(auth_middleware)
+                .route("", web::post().to(steps::set_steps)),
+        );
 }
